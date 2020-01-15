dad359e4933fc237adc525d519664f76a6d8ab61
xavigu <xavigu91@gmail.com>
2020-01-15 16:57:45 +0100

    componentDidMount to fetch json data and create CardList component and render in app.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {CardList} from './components/card-list/card-list.component'
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
-
     this.state = {
-      monsters: [
-        {
-          name: 'Frankestein',
-          id: 1
-        },
-        {
-          name: 'Dracula',
-          id: 2
-        },
-        {
-          name: 'Zombie',
-          id: 3
-        },
-      ]
+      monsters: []
     }
   }
+ componentDidMount(){
+   fetch('https://jsonplaceholder.typicode.com/users')
+        .then(resp => resp.json())
+        .then(users => {
+          console.log(users);
+          this.setState({monsters: users})
+        })
+ }
  
   render() {
     return (
       <div className="App">
+        <CardList name="Peter">
         {
-          this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name}</h1>) //map hace un return de la función que le pasemos iterando por cada objeto del array monsters
+          this.state.monsters.map(monster => 
+                                     <span className="card" key={monster.id}>
+                                        <h1> {monster.name}</h1>
+                                        <p>Email: {monster.email}</p>
+                                        <p>Company: {monster.company.name}</p>
+                                     </span>                              
+                                 ) //map hace un return de la función que le pasemos iterando por cada objeto del array monsters
         }
+        </CardList> 
       </div>
     );
   }
