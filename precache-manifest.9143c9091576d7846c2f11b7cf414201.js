df10db216a2adf21f268f11484962529349507c5
xavigu <xavigu91@gmail.com>
2020-01-16 17:04:14 +0100

    Updates

---
+++ precache-manifest.9143c9091576d7846c2f11b7cf414201.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86fd2dd457844b336cdafb85dc755d6",
+    "url": "/MonsterApp/index.html"
+  },
+  {
+    "revision": "b0dc4b8779107fab317b",
+    "url": "/MonsterApp/static/css/main.7ef12177.chunk.css"
+  },
+  {
+    "revision": "e73bec72a4861f629a2a",
+    "url": "/MonsterApp/static/js/2.02990d15.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MonsterApp/static/js/2.02990d15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0dc4b8779107fab317b",
+    "url": "/MonsterApp/static/js/main.a00727d1.chunk.js"
+  },
+  {
+    "revision": "89dc7eb33658dd0f230e",
+    "url": "/MonsterApp/static/js/runtime-main.764ba9f5.js"
+  }
+]);
